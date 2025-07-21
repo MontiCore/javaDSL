acf065f5284411777937c54727548a6320ada913
Janik Rapp <rapp@se-rwth.de>
2025-07-21 11:00:48 +0200

    set artifact name

--- javaDSL/src/main/java/de/monticore/java/javadsl/_symboltable/JavaDSLScopesGenitor.java
+++ javaDSL/src/main/java/de/monticore/java/javadsl/_symboltable/JavaDSLScopesGenitor.java
@@ -1,18 +1,19 @@
 package de.monticore.java.javadsl._symboltable;
 
-import de.monticore.java.javadsl._ast.ASTCompilationUnit;
-import de.monticore.java.javadsl._ast.ASTOrdinaryCompilationUnit;
-import de.monticore.java.javadsl._ast.ASTPackageDeclaration;
+import de.monticore.java.javadsl.JavaDSLMill;
+import de.monticore.java.javadsl._ast.*;
+import de.monticore.statements.mccommonstatements._ast.ASTConstantsMCCommonStatements;
+import de.monticore.symboltable.ImportStatement;
 
 public final class JavaDSLScopesGenitor extends JavaDSLScopesGenitorTOP {
-
+  
   @Override
   public IJavaDSLArtifactScope createFromAST(ASTCompilationUnit rootNode) {
     IJavaDSLArtifactScope artifactScope = super.createFromAST(rootNode);
-
+    
     if (rootNode instanceof ASTOrdinaryCompilationUnit) {
       ASTOrdinaryCompilationUnit ordinaryCompilationUnit = (ASTOrdinaryCompilationUnit) rootNode;
-
+      
       if (ordinaryCompilationUnit.isPresentPackageDeclaration()) {
         ASTPackageDeclaration packageDeclaration = ordinaryCompilationUnit.getPackageDeclaration();
         artifactScope.setPackageName(packageDeclaration.getMCQualifiedName().getQName());
@@ -20,8 +21,65 @@ public final class JavaDSLScopesGenitor extends JavaDSLScopesGenitorTOP {
     }
 
     // TODO figure out how to map static imports
-
+    
     return artifactScope;
   }
-
+  
+  @Override
+  public void visit(ASTClassDeclaration node) {
+    super.visit(node);
+    if (node.getJavaModifierList().stream()
+        .anyMatch(x -> x.getModifier() == ASTConstantsMCCommonStatements.PUBLIC)) {
+      IJavaDSLScope enclosingScope = node.getEnclosingScope();
+      if (JavaDSLMill.typeDispatcher().isJavaDSLIJavaDSLArtifactScope(enclosingScope)) {
+        enclosingScope.setName(node.getName());
+      }
+    }
+    
+  }
+  
+  @Override
+  public void visit(ASTRecordDeclaration node) {
+    super.visit(node);
+    if (node.getJavaModifierList().stream()
+        .anyMatch(x -> x.getModifier() == ASTConstantsMCCommonStatements.PUBLIC)) {
+      IJavaDSLScope enclosingScope = node.getEnclosingScope();
+      if (JavaDSLMill.typeDispatcher().isJavaDSLIJavaDSLArtifactScope(enclosingScope)) {
+        enclosingScope.setName(node.getName());
+      }
+    }
+  }
+  
+  @Override
+  public void visit(ASTInterfaceDeclaration node) {
+    super.visit(node);
+    if (node.getJavaModifierList().stream()
+        .anyMatch(x -> x.getModifier() == ASTConstantsMCCommonStatements.PUBLIC)) {
+      IJavaDSLScope enclosingScope = node.getEnclosingScope();
+      if (JavaDSLMill.typeDispatcher().isJavaDSLIJavaDSLArtifactScope(enclosingScope)) {
+        enclosingScope.setName(node.getName());
+      }
+    }
+  }
+  
+  @Override
+  public void visit(ASTEnumDeclaration node) {
+    super.visit(node);
+    if (node.getJavaModifierList().stream()
+        .anyMatch(x -> x.getModifier() == ASTConstantsMCCommonStatements.PUBLIC)) {
+      IJavaDSLScope enclosingScope = node.getEnclosingScope();
+      if (JavaDSLMill.typeDispatcher().isJavaDSLIJavaDSLArtifactScope(enclosingScope)) {
+        enclosingScope.setName(node.getName());
+      }
+    }
+  }
+  
+  @Override
+  public void visit(ASTModuleDeclaration node) {
+    super.visit(node);
+    IJavaDSLScope enclosingScope = node.getEnclosingScope();
+    if (JavaDSLMill.typeDispatcher().isJavaDSLIJavaDSLArtifactScope(enclosingScope)) {
+      enclosingScope.setName(node.getName());
+    }
+  }
 }
